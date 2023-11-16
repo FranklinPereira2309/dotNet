b58d75f7d80e76c1d2d6ceb3dcb8180b645db7c5
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-11-16 10:55:01 -0300

    feat:restruturação das pastas da semana1 e semana2

---
+++ semana1/Program.cs
@@ -0,0 +1,121 @@
+﻿
+        Console.WriteLine();
+        Console.WriteLine("Parte 2:");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Valores mínimos e máximos dos tipos de dados numéricos inteiros em C#:");
+
+        Console.WriteLine($"sbyte: {sbyte.MinValue} a {sbyte.MaxValue}");
+        Console.WriteLine($"byte: {byte.MinValue} a {byte.MaxValue}");
+        Console.WriteLine($"short: {short.MinValue} a {short.MaxValue}");
+        Console.WriteLine($"ushort: {ushort.MinValue} a {ushort.MaxValue}");
+        Console.WriteLine($"int: {int.MinValue} a {int.MaxValue}");
+        Console.WriteLine($"uint: {uint.MinValue} a {uint.MaxValue}");
+        Console.WriteLine($"long: {long.MinValue} a {long.MaxValue}");
+        Console.WriteLine($"ulong: {ulong.MinValue} a {ulong.MaxValue}");
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Parte 3:");
+        
+        Console.WriteLine();
+
+        double numeroDouble = 10.9;
+        int numeroCasting = (int)numeroDouble; // Convertido sem a parte fracionária
+        int numeroCastingArred = (int)Math.Round(numeroDouble); // Solução com arredondamento
+
+        Console.WriteLine($"Número double: {numeroDouble}");
+        Console.WriteLine($"Número double convertido sem a parte fracionária: {numeroCasting}");
+        Console.WriteLine($"Número inteiro convertido com arredondamento: {numeroCastingArred}");
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Parte 4:");
+        
+        Console.WriteLine();
+
+        int x = 10;
+        int y = 2;
+        int resultado;
+
+        Console.WriteLine($"Soma: {resultado = x + y}");
+        Console.WriteLine($"Subtração: {resultado = x - y}");
+        Console.WriteLine($"Divisão: {resultado = x / y}");
+        Console.WriteLine($"Multiplicação: {resultado = x * y}");
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Parte 5:");
+        
+        Console.WriteLine();
+
+        int a = 5;
+        int b = 8;
+
+        if (a > b)
+        {
+            Console.WriteLine("a é maior que b");
+        }
+        else if (a < b)
+        {
+            Console.WriteLine("a é menor que b");
+        }
+        else if (a == b)
+        {
+            Console.WriteLine("a é igual a b");
+        }
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Parte 6:");
+        
+        Console.WriteLine();
+
+        string str1 = "Hello";
+        string str2 = "World";
+
+        if (str1.Equals(str2))
+        {
+            Console.WriteLine("As strings SÃO iguais");
+        }
+        else
+        {
+            Console.WriteLine("As strings NÃO são iguais");
+        }
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Parte 7:");
+        
+        Console.WriteLine();
+
+        bool condicao1 = true;
+        bool condicao2 = false;
+
+        if (condicao1 && condicao2)
+        {
+            Console.WriteLine("Ambas as condições são verdadeiras.");
+        }
+        else
+        {
+            Console.WriteLine("Pelo menos uma das condições não é verdadeira.");
+        }
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Parte 8:");
+        
+        Console.WriteLine();
+
+        int num1 = 7, num2 = 3, num3 = 10;
+
+        if (num1 > num2 && num3 == num1 + num2)
+        {
+            Console.WriteLine("Sim - As condições satisfazem o problema!");
+        }
+        else
+        {
+            Console.WriteLine("Não - As condições NÃO satisfazem o problema!");
+        }
+
---
+++ semana1/atividade1.md
@@ -0,0 +1,14 @@
+# Como verificar se o .NET SDK está instalado no computador (Linux):
+
+## dotnet --list-sdks (quais SDK do .NET instaladas);
+## dotnet --list-runtimes (quais runtime .NET instaladas);
+
+# Como remover o .NET SDK no computador linux:
+
+## sudo dotnet uninstall -v <sdk_version>
+## sudo dotnet uninstall -r <runtime_version>
+
+# Como atualizar o .NET SDK no computador linux:
+
+## sudo apt update
+## sudo apt upgrade dotnet-sdk-<sdk_version>
