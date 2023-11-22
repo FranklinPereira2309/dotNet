3fc1ded63d2ec15c7ec2e282734ec62fe3ca96de
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-11-22 10:16:38 -0300

    feat:impelementação de program, app e class produto

--- Semana3/ConsoleApp1/App.cs
+++ Semana3/ConsoleApp1/App.cs
@@ -1,32 +1,48 @@
-using System.Reflection;
-
 namespace produto;
 
 public class App {
     public static List<Produto> AddProduto()
     {
-        List<Produto> produto = new List<Produto>();
+        List<Produto> produtos = new List<Produto>();
         string resposta = "s";
+        
 
         do 
         {
 
-            Console.WriteLine("Digite o nome do produto");
+            Console.WriteLine("Digite o nome do produto: ");
             string nome = Console.ReadLine()!;
-            
+
             try
             {
+                Console.WriteLine("Informe a quantidade em estoque: ");
+                string qtdString = Console.ReadLine()!;
+                int quantidade = int.Parse(qtdString);  
+                Console.WriteLine("Digite o preço unitário: ");
+                string precoString = Console.ReadLine()!;
+                double preco = double.Parse(precoString);
+                Produto produto = new Produto(nome,quantidade,preco);
+                produtos.Add(produto);             
                 
             }
-            catch (System.Exception)
+            catch (FormatException)
             {
                 
-                throw;
+                Console.WriteLine("A quantidade em estoque deve ser um número inteiro.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("A quantidade é muito grande ou muito pequena para ser armazenada.");
+            }
+            finally
+            {
+                Console.WriteLine("Deseja continuar? (s/n): ");
+                resposta = Console.ReadLine()!;
             }
 
         }while(resposta.ToLower() == "s");
 
-        return produto;
+        return produtos;
     }
 
 }
--- Semana3/ConsoleApp1/Program.cs
+++ Semana3/ConsoleApp1/Program.cs
@@ -1,3 +1,15 @@
-﻿using System.Globalization;
+﻿using produto;
+using System.Globalization;
 
 CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("pt-BR");
+
+List<Produto> produtos = App.AddProduto();
+
+foreach (Produto produto in produtos)
+
+{
+    Console.WriteLine($"Código: {produto.CodId}");
+    Console.WriteLine($"Produto: {produto.Nome}");
+    Console.WriteLine($"Quantidade: {produto.Quantidade}");
+    Console.WriteLine($"Preço: {produto.Preco}"); 
+}
