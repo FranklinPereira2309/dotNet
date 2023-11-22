40036dad4aea226ea4cbef5bf1cc4a450600f3e3
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-11-22 09:00:52 -0300

    feat:implementação da classe Produto e Construtuor

--- Semana3/ConsoleApp1/Program.cs
+++ Semana3/ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Semana3!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class Produto {
+    public int Codigo {get; set;}
+    public string Nome {get; set;}
+    public int Quantidade {get; set;}
+
+    public Produto(int _codigo, string _nome, int _quantidade) {
+        Codigo = _codigo;
+        Nome = _nome;
+        Quantidade = _quantidade;
+    }
+    
+}
+
+public class CadastrarProdutos {
+    public List<Produto> Produtos {get; set;}
+}
+
+
